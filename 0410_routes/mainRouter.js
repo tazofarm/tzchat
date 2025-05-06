389219f948a8fd8d7cecd43d59d123984be9731a
tazofarm <tazofarm@gmail.com>
2025-05-06 20:11:27 +0900

    login id 저장

--- 0410_routes/mainRouter.js
+++ 0410_routes/mainRouter.js
@@ -121,7 +121,7 @@ router.get('/users', async (req, res) => {
 router.get('/logout', (req, res) => {
     req.session.destroy(err => {
         if (err) {
-            return res.redirect('/dashboard');
+            return res.redirect('/login');
         }
         res.redirect('/login');
     });
